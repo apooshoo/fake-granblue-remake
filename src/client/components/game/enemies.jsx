34b84226b3761385be8c505e553f35be946aba72
Jonathan <jonathanbek93@gmail.com>
2019-09-08 22:36:41 +0800

    did generate enemy but cannot nail down transition

--- src/client/components/game/enemies.jsx
+++ src/client/components/game/enemies.jsx
@@ -6,6 +6,7 @@ class Enemies extends React.Component {
   constructor() {
     super();
     this.state = {
+        enemiesTransform: [],
         enemies: [],
         laneCoords: []
     }
@@ -17,15 +18,32 @@ class Enemies extends React.Component {
     console.log('targeting lane:', randomLaneIndex+1);
     console.log('lane coords:', randomLaneCoords)
     let {top, left, height, ...others} = randomLaneCoords;
-    let enemy = {
-        active: true,
-        top: top,
-        left: left,
-        height: height
+    let enemyTransform = {
+        x: 1000,
+        duration: 10
     }
-    console.log('generating enemy:', enemy);
+    let enemy = <div
+                    style={{
+                        position: 'absolute',
+                        top: top,
+                        left: left,
+                        height: height,
+                        width: 100,
+                        backgroundColor:'green',
+                        transform: `translateX(${enemyTransform.x}px)`,
+                        transition: `transform ${enemyTransform.duration}s`,
+                    }}
+                />
 
-    this.setState({enemies: [...this.state.enemies].concat(enemy)})
+
+
+    console.log('generating enemy:', enemy);
+    this.setState({
+        enemies: [...this.state.enemies].concat(enemy),
+        enemiesTransform: [...this.state.enemiesTransform.concat(enemyTransform)]
+    })
+
+
   }
 
   componentDidMount(){
@@ -43,24 +61,18 @@ class Enemies extends React.Component {
     console.log("state in enemies:", this.state);
   }
 
+   // .attackingEnemy{
+                //     transition: all 10s linear;
+                //     transform: translateX(1000px);
+                // }
+
   render() {
     let generateEnemies;
-    if(this.state.enemies.length > 0){
-        generateEnemies = [...this.state.enemies].map((enemy, index) => {
-            let {top, left, height} = enemy;
-            return (
-                <div
-                    key={index}
-                    style={{
-                        position: 'absolute',
-                        top: top,
-                        left: left,
-                        height: height,
-                        width: 100,
-                        backgroundColor:'green'
-                    }}
-                />
-            );
+    let enemiesToGenerate = [...this.state.enemies]
+    if(enemiesToGenerate.length > 0){
+        generateEnemies = enemiesToGenerate.map(enemy => {
+            return enemy
+
         });
     } else {
         console.log('no enemies found')
